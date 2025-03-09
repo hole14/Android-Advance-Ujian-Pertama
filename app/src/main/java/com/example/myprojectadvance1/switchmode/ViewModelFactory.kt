6c9cd9798937eed3560757bc6575c7e68e65e6a0
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-03-09 07:50:26 +0700

    Menambahkan

--- app/src/main/java/com/example/myprojectadvance1/switchmode/ViewModelFactory.kt
+++ app/src/main/java/com/example/myprojectadvance1/switchmode/ViewModelFactory.kt
@@ -1,4 +1,15 @@
 package com.example.myprojectadvance1.switchmode
 
-class ViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class ViewModelFactory(private val pref: SettingPreferences): ViewModelProvider.NewInstanceFactory() {
+    @Suppress("UNCHEKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(SettingViewModel::class.java)){
+            return SettingViewModel(pref) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel Class: " + modelClass.name)
+
+    }
 }
