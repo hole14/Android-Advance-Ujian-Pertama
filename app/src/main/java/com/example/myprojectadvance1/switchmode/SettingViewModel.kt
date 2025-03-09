e7f1e9014307ddd4277c11cbeb5712ca788f5185
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-03-09 07:50:13 +0700

    Menambahkan

--- app/src/main/java/com/example/myprojectadvance1/switchmode/SettingViewModel.kt
+++ app/src/main/java/com/example/myprojectadvance1/switchmode/SettingViewModel.kt
@@ -1,4 +1,18 @@
 package com.example.myprojectadvance1.switchmode
 
-class SettingViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class SettingViewModel(private val pref: SettingPreferences) : ViewModel() {
+    fun getThemeSetting(): LiveData<Boolean>{
+        return pref.getThemeSetting().asLiveData()
+    }
+    fun saveThemeSetting(isDarkModeActive: Boolean){
+        viewModelScope.launch {
+            pref.saveThemeSetting(isDarkModeActive)
+        }
+    }
 }
