93dcc51a4b74afd6e0be7ccae7ad7c6a23cd3559
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-03-10 08:33:25 +0700

    Menambahkan

--- app/src/main/java/com/example/myprojectadvance1/data/remote/respone/ListEventResponse.kt
+++ app/src/main/java/com/example/myprojectadvance1/data/remote/respone/ListEventResponse.kt
@@ -1,16 +1,16 @@
-package com.example.myprojectadvance1.data.respone
+package com.example.myprojectadvance1.data.remote.respone
 
 import com.google.gson.annotations.SerializedName
 
 data class ListEventResponse(
 
-	@field:SerializedName("listEvents")
+    @field:SerializedName("listEvents")
 	val listEvents: List<ListEventsItem>,
 
-	@field:SerializedName("error")
+    @field:SerializedName("error")
 	val error: Boolean,
 
-	@field:SerializedName("message")
+    @field:SerializedName("message")
 	val message: String
 )
 
