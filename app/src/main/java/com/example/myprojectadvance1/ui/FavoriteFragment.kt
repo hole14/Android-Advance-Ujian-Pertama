b58ba0920abe692c781ba3502ab2fb829f0e0a0c
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-03-10 08:34:20 +0700

    Menambahkan

--- app/src/main/java/com/example/myprojectadvance1/ui/FavoriteFragment.kt
+++ app/src/main/java/com/example/myprojectadvance1/ui/FavoriteFragment.kt
@@ -5,15 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.StringRes
 import com.example.myprojectadvance1.R
+import com.example.myprojectadvance1.databinding.FragmentFavoriteBinding
+import com.example.myprojectadvance1.favorite.SectionsPagerAdapter
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class FavoriteFragment : Fragment() {
-
+    private lateinit var binding: FragmentFavoriteBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite, container, false)
+        binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val sectionsPagerAdapter = SectionsPagerAdapter(this)
+        binding.viewPager.adapter = sectionsPagerAdapter
+        TabLayoutMediator(
+            binding.tabs, binding.viewPager
+        ) { tab: TabLayout.Tab, position: Int ->
+            tab.text = resources.getString(TAB_TITLES[position])
+        }.attach()
+
+    }
+    companion object {
+        @StringRes
+        private val TAB_TITLES = intArrayOf(R.string.populer, R.string.favorite)
     }
 }
